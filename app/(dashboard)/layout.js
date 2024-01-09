ccdd04fc9155ebccbcb363da8205d5238076f8fc
Umme-hanii <ummehanimohammed9@gmail.com>
2024-01-10 04:37:53 +0530

    Initial sidebar ui

--- app/(dashboard)/layout.js
+++ app/(dashboard)/layout.js
@@ -1,18 +1,19 @@
 import { FaBarsStaggered } from "react-icons/fa6";
 
+import Sidebar from "../components/sidebar";
+
 const DashboardLayout = ({ children }) => {
   return (
     <div className="drawer lg:drawer-open">
-      <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+      <input type="checkbox" id="my-drawer-2" className="drawer-toggle" />
       <div className="drawer-content">
-        {/* Page content here */}
-        <div className="bg-base-400 px-8 py-12 min-h-screen">{children}</div>
         <label
           htmlFor="my-drawer-2"
-          className="primary drawer-button lg:hidden fixed top-6 right-6"
+          className="drawer-button lg:hidden fixed top-6 right-6"
         >
-          <FaBarsStaggered className="w-8 h-8" />
+          <FaBarsStaggered className="w-8 h-8 text-primary" />
         </label>
+        <div className="bg-base-200 px-8 py-12 min-h-screen">{children}</div>
       </div>
       <div className="drawer-side">
         <label
@@ -20,15 +21,7 @@ const DashboardLayout = ({ children }) => {
           aria-label="close sidebar"
           className="drawer-overlay"
         ></label>
-        <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
-          {/* Sidebar content here */}
-          <li>
-            <a>Sidebar Item 1</a>
-          </li>
-          <li>
-            <a>Sidebar Item 2</a>
-          </li>
-        </ul>
+        <Sidebar />
       </div>
     </div>
   );
