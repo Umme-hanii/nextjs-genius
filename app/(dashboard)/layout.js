a4cce5c81a2cdd703bf276c673ae3e986e6e8a21
Umme-hanii <ummehanimohammed9@gmail.com>
2024-01-10 03:59:27 +0530

    Create Dashboard layout

--- app/(dashboard)/layout.js
+++ app/(dashboard)/layout.js
@@ -1,8 +1,35 @@
+import { FaBarsStaggered } from "react-icons/fa6";
+
 const DashboardLayout = ({ children }) => {
   return (
-    <div>
-      <h1>Dashboard Layout</h1>
-      <main>{children}</main>
+    <div className="drawer lg:drawer-open">
+      <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+      <div className="drawer-content">
+        {/* Page content here */}
+        <div className="bg-base-400 px-8 py-12 min-h-screen">{children}</div>
+        <label
+          htmlFor="my-drawer-2"
+          className="primary drawer-button lg:hidden fixed top-6 right-6"
+        >
+          <FaBarsStaggered className="w-8 h-8" />
+        </label>
+      </div>
+      <div className="drawer-side">
+        <label
+          htmlFor="my-drawer-2"
+          aria-label="close sidebar"
+          className="drawer-overlay"
+        ></label>
+        <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
+          {/* Sidebar content here */}
+          <li>
+            <a>Sidebar Item 1</a>
+          </li>
+          <li>
+            <a>Sidebar Item 2</a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
