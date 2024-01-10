4ba958bcfb9bcff9ab7bed2234ca2f1f21e7372a
Umme-hanii <ummehanimohammed9@gmail.com>
2024-01-11 03:01:53 +0530

    Implement MemberProfile section using clerk

--- app/components/MemberProfile.jsx
+++ app/components/MemberProfile.jsx
@@ -1,5 +1,13 @@
-const MemberProfile = () => {
-  return <div>MemberProfile</div>;
+import { UserButton, currentUser } from "@clerk/nextjs";
+
+const MemberProfile = async () => {
+  const user = await currentUser();
+  return (
+    <div className="flex  items-center gap-2 px-4">
+      <UserButton afterSignOutUrl="/" />
+      <p>{user.emailAddresses[0].emailAddress}</p>
+    </div>
+  );
 };
 
 export default MemberProfile;
