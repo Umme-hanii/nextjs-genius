9f2954711403f53e5a8111fddc6fdb19f5736ef7
Umme-hanii <ummehanimohammed9@gmail.com>
2024-01-13 21:31:11 +0530

    React query setup and boilerplate

--- app/(dashboard)/chat/page.js
+++ app/(dashboard)/chat/page.js
@@ -1,10 +1,17 @@
+import {
+  HydrationBoundary,
+  QueryClient,
+  dehydrate,
+} from "@tanstack/react-query";
+
 import Chat from "@/app/components/Chat";
 
 const ChatPage = () => {
+  const queryClient = new QueryClient();
   return (
-    <>
+    <HydrationBoundary state={dehydrate(queryClient)}>
       <Chat />
-    </>
+    </HydrationBoundary>
   );
 };
 
---
+++ utils/actions.js
@@ -0,0 +1,7 @@
+"use server";
+
+export const generateChatResponse = async (chatMessage) => {
+  console.log(chatMessage);
+
+  return "awesome";
+};
