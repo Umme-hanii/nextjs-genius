c4072673e0349c92e2e528dc95bf7a305d4021f0
Umme-hanii <ummehanimohammed9@gmail.com>
2024-01-14 21:59:14 +0530

    Install openai and make a request to openai API

--- utils/actions.js
+++ utils/actions.js
@@ -1,7 +1,21 @@
 "use server";
+import OpenAI from "openai";
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
 
 export const generateChatResponse = async (chatMessage) => {
-  console.log(chatMessage);
+  const response = await openai.chat.completions.create({
+    messages: [
+      { role: "system", content: "You are a helpful assistant" },
+      { role: "user", content: chatMessage },
+    ],
+    model: "gpt-3.5-turbo",
+    temperature: 0,
+  });
+  console.log("RESPONSE.CHOICES[0]", response.choices[0]);
+  console.log("ENTIRE RESPONSE", response);
 
   return "awesome";
 };
