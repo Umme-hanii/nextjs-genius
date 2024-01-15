c97a64d7ffdbef20820a7f606bb151637dc8dbf1
Umme-hanii <ummehanimohammed9@gmail.com>
2024-01-15 19:26:04 +0530

    Refactoring

--- app/(dashboard)/layout.js
+++ app/(dashboard)/layout.js
@@ -11,7 +11,7 @@ const DashboardLayout = ({ children }) => {
           htmlFor="my-drawer-2"
           className="drawer-button lg:hidden fixed top-6 right-6"
         >
-          <FaBarsStaggered className="w-8 h-8 text-primary" />
+          <FaBarsStaggered className="w-8 h-8 text-secondary" />
         </label>
         <div className="bg-base-200 px-8 py-12 min-h-screen">{children}</div>
       </div>
