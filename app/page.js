7560198fb2f7c76944548a0acea618cd45f6cce8
Umme-hanii <ummehanimohammed9@gmail.com>
2024-01-16 21:15:50 +0530

    Title change

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ const HomePage = () => {
       <div className="hero-content max-w-5xl mockup-window border bg-neutral text-secondary">
         <div className="max-w-md">
           <div className="flex justify-center flex-col px-4 py-16 bg-base-200">
-            <h1 className="text-5xl font-bold text-primary">Next Genius</h1>
+            <h1 className="text-5xl font-bold text-primary">Tour Genius</h1>
 
             <p className="py-6 text-lg leading-loose text-base-content">
               Tour Genius: Your AI language companion. Powered by OpenAI, it
